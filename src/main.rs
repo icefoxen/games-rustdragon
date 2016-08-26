425101cd1a30a761b98a738e3f2a09771df257ac
Simon Heath <icefoxen@gmail.com>
2016-08-25 21:55:37 -0400

    Fixed annoying inconsistency in status printing.

--- src/main.rs
+++ src/main.rs
@@ -380,27 +380,31 @@ fn run_turn(field: &mut Battlefield, actions: &mut Vec<Action>) -> BattleStatus 
 
         run_action(field, &action);
     }
-    println!("");
+
+    // Check again, juuuuust in case that last action finished
+    // something off.
+    if field.team_victorious(Team::Player) {
+        return BattleStatus::PlayerVictory;
+    }
+    else if field.team_victorious(Team::Monster) {
+        return BattleStatus::MonsterVictory;
+    }
+
+    
     field.increment_round();
     BattleStatus::Continuing
 }
 
-
-fn main() {
-    let c1 = Character::new("Ragnar", Team::Player);
-    let mut c2 = Character::new("Alena", Team::Player);
-    c2.spd = 100;
-    let m1 = Character::new("Slime", Team::Monster);
-    let m2 = Character::new("Bat", Team::Monster);
-    let mut b = Battlefield::new();
-    b.chars = vec![c1, c2, m1, m2];
+fn mainloop(mut field: Battlefield) {
     let a1 = Action::Attack(0, 2);
     let a2 = Action::Defend(2);
     let a3 = Action::Attack(1, 2);
     let mut actions1 = vec![a1, a2, a3];
+
     loop {
-        println!("{}", b);
-        match run_turn(&mut b, &mut actions1) {
+        println!("");
+        println!("{}", field);
+        match run_turn(&mut field, &mut actions1) {
             BattleStatus::PlayerVictory => {
                 println!("Victory!\n");
                 break;
@@ -412,4 +416,16 @@ fn main() {
             _ => ()
         }
     };
+}
+
+
+fn main() {
+    let c1 = Character::new("Ragnar", Team::Player);
+    let mut c2 = Character::new("Alena", Team::Player);
+    c2.spd = 100;
+    let m1 = Character::new("Slime", Team::Monster);
+    let m2 = Character::new("Bat", Team::Monster);
+    let mut b = Battlefield::new();
+    b.chars = vec![c1, c2, m1, m2];
+    mainloop(b);
 }
